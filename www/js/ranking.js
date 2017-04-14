ca5e41ea4833b9200ec4646ff9083563ef66e6b5
oonoyosp <oonoyosp@systena.co.jp>
2017-04-14 17:56:03 +0900

    fix Copyright name for LICENSE and java doc

--- www/js/ranking.js
+++ www/js/ranking.js
@@ -3,7 +3,7 @@
 //  MonacaFirstApp
 //
 //  Created by Natsumo Ikeda on 2016/07/01.
-//  Copyright © 2016年 NIFTY Corporation. All rights reserved.
+//  Copyright 2017 FUJITSU CLOUD TECHNOLOGIES LIMITED All Rights Reserved.
 //
 
 // 「ランキングを見る」ボタン押下時の処理
